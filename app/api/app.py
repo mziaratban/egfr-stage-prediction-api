af23fb3559217158f6e7f4612db5bd88524415be
Majid <ziaratban@gmail.com>
2026-07-15 06:20:42 -0700

    Monitoring added

--- app/api/app.py
+++ app/api/app.py
@@ -3,8 +3,26 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 import os
+# for monitoring
+from prometheus_fastapi_instrumentator import Instrumentator
+from .middleware import PredictionMetricsMiddleware
+from .metrics import (
+    prediction_total,
+    prediction_stage_counter,
+    prediction_failures,
+)
+
+
 
 app = FastAPI(title="eGFR Prediction API", version="1.0")
+
+app.add_middleware(PredictionMetricsMiddleware)
+
+Instrumentator().instrument(app).expose(app)
+
+
+
+
 
 
 # -----------------------
@@ -61,16 +79,35 @@ def version():
 @app.post("/predict", response_model=PredictionOutput)
 def predict(data: InputData):
 
-    x = np.array(data.features, dtype=float).reshape(1, -1)
+    try:
+        prediction_total.inc()
 
-    preds = [
-        5-int(model_1.predict(x)[0]),
-        5-int(model_2.predict(x)[0]),
-        5-int(model_3.predict(x)[0]),
-        5-int(model_4.predict(x)[0]),
-        5-int(model_5.predict(x)[0]),
-        5-int(model_6.predict(x)[0]),
-        5-int(model_7.predict(x)[0]),
-    ]
+        x = np.array(data.features, dtype=float).reshape(1, -1)
 
-    return {"predictions": preds}
+        preds = [
+            5-int(model_1.predict(x)[0]),
+            5-int(model_2.predict(x)[0]),
+            5-int(model_3.predict(x)[0]),
+            5-int(model_4.predict(x)[0]),
+            5-int(model_5.predict(x)[0]),
+            5-int(model_6.predict(x)[0]),
+            5-int(model_7.predict(x)[0]),
+        ]
+
+        
+        for day, stage in enumerate(preds, start=1):
+
+            prediction_stage_counter.labels(
+                day=str(day),
+                stage=str(stage)
+            ).inc()
+    
+    
+        return {"predictions": preds}
+
+
+    except Exception:
+
+        prediction_failures.inc()
+
+        raise
